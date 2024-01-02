f2df9ea2784006090d13b99c3deacda417d846b1
░▒▓█│【Walkman】│█▓▒░ <matthewc@direwolf.co.za>
2024-01-02 16:10:57 +0200

    added project

---
+++ Properties/Application.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+
+namespace RunAsUser {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
